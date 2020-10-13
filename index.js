8f32899a65e4e784580773a4b20689098b344885
Maxim Chernykh <max_kray@list.ru>
2020-10-13 12:01:59 +0300

    pinger

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const { MinecraftQuery } = require("minecraft-status");
 
 const app = express();
 const PORT = 80;
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.send("It worksss max");
+  function cb(error, response) {
+    if (error) throw error;
+    else console.log(response);
+  }
+  const result = await MinecraftQuery.fullQuery("mc.gladcraft.ru", 25565, cb);
+  console.log(result);
 });
 
 app.listen(PORT, () => {
